6fb58bdda0ca62e5ae6e5401d0135e16eadd5487
KarvaMies <jeremias.moberg@gmail.com>
2023-02-17 14:37:17 +0200

    Task 2

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react"
+
 function About() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    }, [])
+
   return (
     <div>
         <h1>About this website</h1>
-        <p>This is just a regular website to demonstrate my abilities to make a simple react website</p>
+        <ul>
+            {data.map((item) => (
+                <li key={item.id}>{item.title}</li>
+            ))}
+        </ul>
     </div>
   )
 }
