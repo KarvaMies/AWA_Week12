e052daf9f17bf39a2b5bbad739faa56491c930a5
KarvaMies <jeremias.moberg@gmail.com>
2023-02-17 15:04:53 +0200

    Task 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Button } from "@mui/material";
+import { useTranslation } from "react-i18next";
+
 function Header() {
+  const { t } = useTranslation();
+
   return (
-    <div>
-        <h1>This is header</h1>
-    </div>
-  )
+    <AppBar position="static">
+      <Toolbar>
+        <Button component={Link} to="/" color="inherit">
+          {t("home")}
+        </Button>
+        <Button component={Link} to="/about" color="inherit">
+          {t("about")}
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default Header
+export default Header;
